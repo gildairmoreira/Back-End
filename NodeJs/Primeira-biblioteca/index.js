eb91d6d61209f393726ad744639799fa0b7e3211
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-09-24 20:15:13 -0300

    regex

--- NodeJs/Primeira-biblioteca/index.js
+++ NodeJs/Primeira-biblioteca/index.js
@@ -7,7 +7,40 @@ function trataErro(erro)
   throw new Error(chalk.red(erro.code, 'Não Há arquivo no diretorio'))
 }
 
+function extraiLinks(texto)
+{
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+}
+
+
+//assync/await
+async function pegaArquivo(caminhoDoArquivo)
+{
+  try
+  {
+    const encoding = 'utf8';
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(chalk.green(texto))
+
+  } catch (erro)
+  {
+    trataErro(erro)
+  }
+
+
+}
+
+/* //promeses com then
 function pegaArquivo(caminhoDoArquivo)
+{
+  const encoding = 'utf8';
+  fs.promises
+    .readFile(caminhoDoArquivo, encoding)
+    .then((texto) => console.log(chalk.green(texto)))
+    .catch(trataErro)
+} */
+
+/* function pegaArquivo(caminhoDoArquivo)
 {
   const encoding = 'utf8';
   fs.readFileSync(caminhoDoArquivo, encoding, (erro, texto) =>
@@ -21,5 +54,8 @@ function pegaArquivo(caminhoDoArquivo)
 
   });
 }
-
+ */
 pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
+
+// \(https?:\/\/[^\s?#.].[^\s]*\)
