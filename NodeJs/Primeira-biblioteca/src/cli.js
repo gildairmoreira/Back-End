6f782cdae86b99513469320b8979373639b8b0bc
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-09-28 09:00:44 -0300

    validação http

--- NodeJs/Primeira-biblioteca/src/cli.js
+++ NodeJs/Primeira-biblioteca/src/cli.js
@@ -1,25 +1,58 @@
-import chalk from "chalk";
-import pegaArquivo from "./index.js";
+import chalk from 'chalk';
 import fs from 'fs';
-
+import pegaArquivo from './index.js';
+import listaValidada from './http-validacao.js';
 
 const caminho = process.argv;
 
+async function imprimeLista(valida, resultado, identificador = '')
+{
+  if (valida)
+  {
+    console.log(
+      chalk.yellow('lista validada'),
+      chalk.black.bgGreen(identificador),
+      await listaValidada(resultado));
+  } else
+  {
+    console.log(
+      chalk.yellow('lista de links'),
+      chalk.black.bgGreen(identificador),
+      resultado);
+  }
+}
 
-async function processaTexto (argumentos) {
-  
-  if (fs.lstatSync(caminho).isFile()) {
-    const caminho = argumentos[2]
+
+async function processaTexto(argumentos)
+{
+  const caminho = argumentos[ 2 ];
+  const valida = argumentos[ 3 ] === '--valida';
+
+  try
+  {
+    fs.lstatSync(caminho);
+  } catch (erro)
+  {
+    if (erro.code === 'ENOENT')
+    {
+      console.log('arquivo ou diretório não existe');
+      return;
+    }
+  }
+
+  if (fs.lstatSync(caminho).isFile())
+  {
     const resultado = await pegaArquivo(argumentos[ 2 ]);
-    console.log(chalk.yellow('lista de links'),resultado)
-    
-  } else if (fs.lstatSync(caminho).isDirectory()){
-    const arquivos = await fs.promises.readdir(caminho);
-    arquivos.forEach((nomeDeArquivo) => {
-      const lista = pegaArquivo(`${caminho}/${nomeDeArquivo}`)
-      console.log(lista) 
+    imprimeLista(valida, resultado);
+  } else if (fs.lstatSync(caminho).isDirectory())
+  {
+    const arquivos = await fs.promises.readdir(caminho)
+    arquivos.forEach(async (nomeDeArquivo) =>
+    {
+      const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
+      imprimeLista(valida, lista, nomeDeArquivo)
     })
   }
 }
 
-processaTexto(caminho)
+processaTexto(caminho);
