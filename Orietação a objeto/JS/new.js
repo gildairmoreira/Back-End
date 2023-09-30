6af8995914979bcac852e6db6fdeabfb7ac05c9f
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-09-30 18:04:35 -0300

    function construct

---
+++ Orietação a objeto/JS/new.js
@@ -0,0 +1,21 @@
+function User(nome,email) {
+  this.nome = nome,
+  this.email = email
+
+  this.exibirInfos = () => {
+    return `${this.nome} ${this.email}`
+  }
+}
+
+//const novoUser = new User('GILDAIR', 'gildair@g.com')
+//console.log(novoUser.exibirInfos())
+
+function Admin(role) {
+  User.call(this, 'GILDAIR', 'gildair@g.com')
+  this.role = role || 'estudante'
+}
+
+Admin.prototype = Object.create(User.prototype)
+const novoUser = new Admin('admin')
+console.log(novoUser.exibirInfos())
+console.log(novoUser.role)
