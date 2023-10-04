abb7046cf9c91527cd4055dd15ccf82baf15b82d
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-10-04 10:27:12 -0300

    aprendendo a criar apis com node e express

--- NodeJs/api-express-mongodb/server.js
+++ NodeJs/api-express-mongodb/server.js
@@ -1,10 +1,15 @@
 import http from 'http'
 
 const PORT = 3000
+const rotas = {
+  "/": "Curso De Node.JS",
+  "/livros": "Curso De livros.JS",
+  "/autores": "Curso De autor.JS",
+}
 
 const serve = http.createServer((req, res) => {
   res.writeHead(200, {"Content-Type": "text/plain"})
-  res.end("Curso de Nodes.js")
+  res.end(rotas[req.url])
 })
 
 serve.listen(PORT, () => {
