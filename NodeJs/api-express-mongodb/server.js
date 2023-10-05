0141251c790518b39e050ff35552a7efe9ae8f23
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-10-05 14:22:29 -0300

    instalando mongodb

--- NodeJs/api-express-mongodb/server.js
+++ NodeJs/api-express-mongodb/server.js
@@ -1,17 +1,8 @@
-import http from 'http'
+import app from "./src/app.js"
 
 const PORT = 3000
-const rotas = {
-  "/": "Curso De Node.JS",
-  "/livros": "Curso De livros.JS",
-  "/autores": "Curso De autor.JS",
-}
 
-const serve = http.createServer((req, res) => {
-  res.writeHead(200, {"Content-Type": "text/plain"})
-  res.end(rotas[req.url])
-})
 
-serve.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("Server listening on")
 })
---
+++ NodeJs/api-express-mongodb/src/app.js
@@ -0,0 +1,58 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json())
+
+const livros = [
+  {
+    id: 1,
+    titulo: 'O senho dos aneis'
+  },
+  {
+    id: 2,
+    titulo: 'O Hobit'
+  }
+]
+
+function buscaLivros(id) {
+  return livros.findIndex(livro => {
+    return livro.id === Number(id)
+  })
+}
+
+
+app.get('/', (req, res) =>
+{
+  res.status(200).send('Cueso de nodeJS')
+})
+
+app.get('/livros', (req, res) =>
+{
+  res.status(200).json(livros)
+})
+
+app.get('/livros/:id', (req, res) =>
+{
+ const index = buscaLivros(req.params.id)
+ res.status(200).json(livros[index])
+})
+
+app.post('/livros', (req, res) =>{
+  livros.push(req.body)
+  res.status(201).send('livro cadastrado com sucesso')
+})
+
+app.put("/livros/:id", (req,res) =>{
+  const index = buscaLivros(req.params.id)
+  livros[index].titulo = req.body.titulo
+  res.status(200).json(livros)
+})
+
+app.delete("/livros/:id", (req,res) => {
+  const index = buscaLivros(req.params.id)
+  livros.splice(index, 1)
+  res.status(200).send('livro removido Com sucesso')
+})
+
+
+export default app
