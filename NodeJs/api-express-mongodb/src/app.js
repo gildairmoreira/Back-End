c402f5e69ede9af00a0c56d2a6a18aee458b1483
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-10-06 21:47:52 -0300

    corrigindo erro

--- NodeJs/api-express-mongodb/src/app.js
+++ NodeJs/api-express-mongodb/src/app.js
@@ -1,46 +1,24 @@
-import express from 'express';
-import conectaNaDataBase from './config/dbConnect.js';
-import routes from './Routes/index.js';
+import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import routes from "./routes/index.js";
 
-
-const conexao = await conectaNaDataBase()
+const conexao = await conectaNaDatabase();
 
 conexao.on("error", (erro) => {
-  console.error("Erro de Conexão", erro);
-})
+  console.error("erro de conexão", erro);
+});
 
 conexao.once("open", () => {
-  console.log("Conexão feita com sucesso")
+  console.log("Conexao com o banco feita com sucesso");
 })
-
 
 const app = express();
+routes(app);
 
-routes(app)
+app.delete("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros.splice(index, 1);
+  res.status(200).send("livro removido com sucesso");
+});
 
-app.get('/livros/:id', (req, res) =>
-{
- const index = buscaLivros(req.params.id)
- res.status(200).json(livros[index])
-})
-
-app.post('/livros', (req, res) =>{
-  livros.push(req.body)
-  res.status(201).send('livro cadastrado com sucesso')
-})
-
-app.put("/livros/:id", (req,res) =>{
-  const index = buscaLivros(req.params.id)
-  livros[index].titulo = req.body.titulo
-  res.status(200).json(livros)
-})
-
-app.delete("/livros/:id", (req,res) => {
-  const index = buscaLivros(req.params.id)
-  livros.splice(index, 1)
-  res.status(200).send('livro removido Com sucesso')
-})
-
-
-export default app
-
+export default app;
