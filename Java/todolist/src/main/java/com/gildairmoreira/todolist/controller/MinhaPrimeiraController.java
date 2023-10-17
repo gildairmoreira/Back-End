1933eb69087e129885ace11979461fa7935f2195
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-10-17 15:26:42 -0300

    controller

--- Java/todolist/src/main/java/com/gildairmoreira/todolist/controller/MinhaPrimeiraController.java
+++ Java/todolist/src/main/java/com/gildairmoreira/todolist/controller/MinhaPrimeiraController.java
@@ -1,12 +1,11 @@
 package com.gildairmoreira.todolist.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 
 public class MinhaPrimeiraController {
   public String primeiraMensagem() {
     return "Funcionou";
-    
+  
   }
 }
