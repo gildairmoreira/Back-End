675a4432d4c2ed89d4b39b9ad39bc5e9bcc0e6d5
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-11-13 15:19:10 -0300

    ada 2

---
+++ Java/Codders/demo/src/main/java/com/codderssantander/App.java
@@ -0,0 +1,15 @@
+package com.codderssantander;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String nome = "gildair";
+        System.out.println( "Hello World! " + nome );
+        System.out.println(nome.length());
+    }
+}
