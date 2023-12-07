4e34f9c2e2442f8ed9836c74679e8a6c404508b8
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-12-07 18:33:40 -0300

    Aprendendo sobre palavras reservadas no java

--- Java/Codders/poo/src/Main.java
+++
@@ -1,7 +0,0 @@
-import Animais.Cachorro;
-
-public class Main {
-    public static void main(String[] args) {
-        Cachorro cachorro1 = new Cachorro();
-    }
-}
--- Java/EBAC/Contruindo Classes/src/PrimeiraClasse.java
+++ Java/EBAC/Contruindo Classes/src/PrimeiraClasse.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class PrimeiraClasse {
+public class PrimeiraClasse {
+    public static  void  main(String args[]){
+
+        Cliente cliente = new Cliente();
+        cliente.cadastrarEndereco("Rua quinze");
+        cliente.setCodigo(1);
+        System.out.println(cliente.getEndereco());
+        System.out.println(cliente.getCodigo());
+    }
 }
