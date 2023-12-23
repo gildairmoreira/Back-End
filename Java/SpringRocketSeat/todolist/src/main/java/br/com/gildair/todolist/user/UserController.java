635ad8fc34f00499eba880b88b7d90e1db4a30ea
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-12-23 19:15:57 -0300

    Criando usuario No banco de dados

--- Java/SpringRocketSeat/todolist/src/main/java/br/com/gildair/todolist/user/UserController.java
+++ Java/SpringRocketSeat/todolist/src/main/java/br/com/gildair/todolist/user/UserController.java
@@ -3,27 +3,20 @@ package br.com.gildair.todolist.user;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-
-/**
- * Modificador public private protected
- */
 
 @RestController
 @RequestMapping("/users")
-
 public class UserController {
 
-    /**
-     * String (texto) Integer (int) Numeros inteiros Double (double) Números 0.00
-     * Float (float) Números 0.00 char (A C) Date (data) void
-     */
-
+    @Autowired
+    private IUserRepository userRepository;
 
     @PostMapping("/")
-
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 
 }
