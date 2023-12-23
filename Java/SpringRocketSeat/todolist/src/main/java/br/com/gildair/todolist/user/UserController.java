140103143c3c415b88c01c617805f5dbc855343d
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-12-23 15:58:45 -0300

    Aprendendo a usar LOMBOK para otimizar Geters e Setters

--- Java/SpringRocketSeat/todolist/src/main/java/br/com/gildair/todolist/user/UserController.java
+++ Java/SpringRocketSeat/todolist/src/main/java/br/com/gildair/todolist/user/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
 /**
  * Modificador public private protected
  */
@@ -20,13 +19,11 @@ public class UserController {
      * Float (float) Números 0.00 char (A C) Date (data) void
      */
 
-    /**
-     * Body
-     */
 
-     @PostMapping("/")
-     
+    @PostMapping("/")
+
     public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+        System.out.println(userModel.getUsername());
     }
+
 }
--- Java/SpringRocketSeat/todolist/src/main/java/br/com/gildair/todolist/user/UserModel.java
+++ Java/SpringRocketSeat/todolist/src/main/java/br/com/gildair/todolist/user/UserModel.java
@@ -1,7 +1,18 @@
 package br.com.gildair.todolist.user;
 
+import lombok.Data; //Faz GET e SET das variaveis
+// import lombok.Getter; //Faz somente GET das variaveis
+// import lombok.Setter; //Faz somente SET das variaveis
+
+/* para não ter que declarar cada get e set usa-se a lib Lombok  */
+
+@Data
 public class UserModel {
-    public String userName;
-    public String name;
-    public String password;
+    private String username;
+    private String name;
+    private String password;
+
+    // getters e setters
+    
+
 }
