bbd41720fe468f02f795f6d41463c54baf81fda9
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-12-28 17:57:52 -0300

    Adicionando tratamento de erros

--- Java/SpringRocketSeat/todolist/src/main/java/br/com/gildair/todolist/task/TaskModel.java
+++ Java/SpringRocketSeat/todolist/src/main/java/br/com/gildair/todolist/task/TaskModel.java
@@ -43,4 +43,11 @@ public class TaskModel {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception {
+        if(title.length() >  50){
+            throw new Exception("O Campo Title deve ter no maximo 50 Cacteres");
+        }
+        this.title = title;
+    }
 }
