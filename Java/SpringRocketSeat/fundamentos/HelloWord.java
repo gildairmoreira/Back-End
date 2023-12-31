6fbcbddb8b8f5b7935b41bd207a57150988a75fc
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-12-31 18:59:33 -0300

    começando a fazer o curso do ignite JAVA: Fundamentos (relembrando)

---
+++ Java/SpringRocketSeat/fundamentos/HelloWord.java
@@ -0,0 +1,16 @@
+/* 
+ * HelloWord = Nome da classe
+ * public = tipo de acesso da classe
+ * class = tipo da classe
+ */
+
+public class HelloWorld {
+    //Todo conteúdo deverá ser inserido aqui dentro
+    // static = fala que o main pertençe a classe
+    //void = tipo de retorno da classe
+
+    public static void main(String[] args){
+
+    }
+}
+//Fora do escopo da classe
