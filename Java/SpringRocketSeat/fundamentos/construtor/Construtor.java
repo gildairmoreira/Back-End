2aea13054d3500e474b1ba38515f3f521c196742
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2024-01-02 16:01:24 -0300

    Terminei modulo de fundamentos Java Ignite RocketSeat

---
+++ Java/SpringRocketSeat/fundamentos/construtor/Construtor.java
@@ -0,0 +1,18 @@
+package construtor;
+
+public class Construtor {
+    private int numero;
+    
+    public Construtor(int numero, String texto) {
+        this.numero = numero;
+    }
+
+
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+}
---
+++ Java/SpringRocketSeat/fundamentos/wrapper/Warapper.java
@@ -0,0 +1,13 @@
+package wrapper;
+
+public class Warapper {
+    public static void main(String[] args) {
+        int tipoint = 1;
+        float tipoFloat = 1;
+        boolean tipoBoolean = true;
+
+        Float tipoFloat2 = 1F;
+        Boolean tipoint2 = false;
+        Integer tipoBoolean2 = null;
+    }
+}
