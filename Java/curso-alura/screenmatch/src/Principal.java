423edb0d8431f22d9bedc139733ddf804a689edc
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2024-04-08 16:11:04 -0300

    Iniciando curso de OO em Java da Oracle

---
+++ Java/curso-alura/screenmatch/src/Principal.java
@@ -0,0 +1,19 @@
+import br.com.gildairoo.screenmatch.modelos.Filme;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println("Total De Avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+    }
+}
+
